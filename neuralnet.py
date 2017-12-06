b394dfd1de48303702189746ae2776912474b59b
Johny Rufus <johnjoh@iu.edu>
2017-12-06 11:26:48 -0800

    Neural Net: Add backward propagation

--- neuralnet.py
+++ neuralnet.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 from collections import defaultdict
+import unittest
 
 from algorithm import MLAlgorithm
 
@@ -16,7 +17,7 @@ class NeuralNet(MLAlgorithm):
 
         def sigmoid(x): return 1 / (1 + np.exp(-x))
 
-        layers_nodes = {0:192, 1:10, 2:4}
+        layers_nodes = {0:192, 1:50, 2:4}
         layers = len(layers_nodes)
         # weights = [ np.random.uniform(low=-1/np.sqrt(layers_nodes[i-1]), high=1/np.sqrt(layers_nodes[i-1]),
         # size=layers_nodes[i-1]) for i in range(layers-1)]
@@ -35,46 +36,94 @@ class NeuralNet(MLAlgorithm):
             a = []
             for j, feature in enumerate(features):
                 #print(j)
-                a.append(self.forward_prop(feature, layers, layers_nodes, weights, sigmoid))
+                inputv = defaultdict(list)
+                a.append(self.forward_prop(feature, layers, layers_nodes, weights, sigmoid, inputv))
 
-    def forward_prop(self, feature, layers, layers_nodes, weights, activation):
+    def forward_prop(self, feature, layers, layers_nodes, weights, activation, inputv):
         a = defaultdict(list)
 
         a[0] = feature
+        inputv[0].append(feature)
         for l in range(1, layers):
             for node in range(layers_nodes[l]):
-                a[l].append(activation(np.dot(weights[l][node], a[l - 1])))
+                inputv[l].append(np.dot(weights[l][node], a[l - 1]))
+                a[l].append(activation(inputv[l][node]))
 
-            '''a[l] = [activation(np.dot(weights[l][node], a[l - 1])) for node in range(layers_nodes[l]-1)]
-               if l != layers-1:
-               a[l].append(1)
-            '''
+            #a[l] = [activation(np.dot(weights[l][node], a[l - 1])) for node in range(layers_nodes[l]-1)]
+            '''if l != layers-1:
+                a[l].append(1)
+                inputv[l].append(1)'''
         return a
 
+    def backward_prop(self, a, y, feature, layers, layers_nodes, weights, activation, derivative, inputv):
 
-    def test_forward_prop(self):
-        layers = 3
-        layers_nodes = {0: 3, 1: 2, 2: 1}
-        weights = {1: {0:np.array([0.5, 0.5, 0.5]),
-                       1:np.array([0.1, 0.2, 0.3]),},
-                       #2:np.array([0.5, 0.5, 0.5, 0.5]),
-                       #3:np.array([0.5, 0.5, 0.5, 0.5])},
-                   2: {0:np.array([0.1, 0.2,]),}}
-                       #1:np.array([0.5, 0.5, 0.5, 0.5]),
-                       #2: np.array([0.1, 0.5, 0.5, 0.5])}}
-        feature = np.array([2, 3, 4,])
-        a = self.forward_prop(feature, layers, layers_nodes, weights, lambda x: x)
-        print(a)
-        assert a[0] == [2,3,4]
-        assert a[1] == [4.5, 2.0]
-        assert a[2] == [0.85000000000000009]
+        delta = defaultdict(list)
 
+        '''for j in range(layers_nodes[layers - 1]-1):
+            delta[layers - 1] = []
+            inputv[layers - 1]
+            print(inputv[layers - 1], layers-1, j)
+            inputv[layers - 1][j]
+            derivative(inputv[layers - 1][j]) * 1
+            y[j] - a[layers - 1][j]'''
+
+
+        delta[layers-1] = [derivative(inputv[layers-1][j]) * (y[j]-a[layers-1][j]) for j in range(layers_nodes[layers-1])]
+        #print(delta)
+
+        for layer in range(layers-2, 0, -1):
+            #print(delta)
+            #print(weights)
+            delta[layer] = [0] * layers_nodes[layer]
+            for i in range(layers_nodes[layer]):
+                total = 0
+                for j in range(layers_nodes[layer + 1]):
+                    #print(i, j)
+                    total += weights[layer+1][j][i] * delta[layer + 1][j]
+                delta[layer][i] = derivative(inputv[layer][i]) * total
+
+        for layer in range(layers-1):
+            for i in range(layers_nodes[layer]):
+                for j in range(layers_nodes[layer + 1]):
+                    weights[layer+1][j][i] = weights[layer+1][j][i] + 0.1 * a[layer][i] * delta[layer+1][j]
+
+        #print(delta)
 
     def test(self):
        pass
+
+class NeuralNetTest(unittest.TestCase):
+    def test(self):
+        layers = 3
+        layers_nodes = {0: 3, 1: 2, 2: 1}
+        weights = {1: {0: np.array([0.5, 0.5, 0.5]),
+                       1: np.array([0.1, 0.2, 0.3]), },
+                   # 2:np.array([0.5, 0.5, 0.5, 0.5]),
+                   # 3:np.array([0.5, 0.5, 0.5, 0.5])},
+                   2: {0: np.array([0.1, 0.2, ]), }}
+        # 1:np.array([0.5, 0.5, 0.5, 0.5]),
+        # 2: np.array([0.1, 0.5, 0.5, 0.5])}}
+        feature = np.array([2, 3, 4, ])
+
+        train_file = 'train-data.txt'
+        model_file = 'model_file_nn.txt'
+        nn = NeuralNet(train_file, model_file)
+
+        a = nn.forward_prop(feature, layers, layers_nodes, weights, lambda x: x, defaultdict(list))
+        print(a)
+
+        self.assertCountEqual(a[0], [2, 3, 4])
+        self.assertCountEqual(a[1], [4.5, 2.0])
+        self.assertCountEqual(a[2], [0.85000000000000009])
+
 
 
 train_file = 'train-data.txt'
 model_file = 'model_file_nn.txt'
 nn = NeuralNet(train_file, model_file)
-nn.test_forward_prop()
+nn.train()
+
+# Run test every time
+
+nn_test = NeuralNetTest()
+nn_test.test()
